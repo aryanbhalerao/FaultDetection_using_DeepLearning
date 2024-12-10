85b2e5e90929b20244bc8aaaa406cb031f300780
Aryan Bhalerao <aryan.bhalerao@protonmail.com>
2024-12-10 11:30:12 +0530

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.title("Fault Detection for Tennesse Eastmann using LSTM")
 # Add a button
 if st.button("Run Model"):
     # Load and display the image
-    image = Image.open("result.jpg")  # Replace 'example.jpg' with your image path
+    image = Image.open("result.png")  # Replace 'example.jpg' with your image path
     st.image(image, caption="Result after training with LSTM model", use_column_width=True)
 else:
     st.write("Run LSTM Model")
