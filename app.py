58af8b8ee3f5de2226b6c7c092e8c7ec16ce5c43
Aryan Bhalerao <aryan.bhalerao@protonmail.com>
2024-12-10 11:28:43 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from PIL import Image
+
+# Set the title of the app
+st.title("Fault Detection for Tennesse Eastmann using LSTM")
+
+# Add a button
+if st.button("Run Model"):
+    # Load and display the image
+    image = Image.open("result.jpg")  # Replace 'example.jpg' with your image path
+    st.image(image, caption="Result after training with LSTM model", use_column_width=True)
+else:
+    st.write("Run LSTM Model")
